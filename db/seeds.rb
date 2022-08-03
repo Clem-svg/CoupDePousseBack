a45aa027a51dcd44f8ec40be007e1b16ca7fa82e
Clem-Svg <clementine.fouque@gmail.com>
2022-08-03 13:26:09 +0200

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,33 +17,25 @@ User.first_or_create(
 )
 
 Garden.create!(
-  title: "Jardin de #{Faker::Superhero.name} besoin d'aide urgente pour planter des choux",
-  description: [
-    "Charmant jardin à partager rempli de camélias et myosotis.
-    Besoin d'une main experte pour m'aider à les chouchouter",
-    "Jardin dans un état déplorable, laissé à l'abandon par mes parents dans les années 70. J'ai besoin d'aide ASAP",
-    'Je voudrais planter un potager dans ce jardinet. Hors de question de remettre les pieds dans un supermarché',
-    "Besoin d'aide pour entretenir ce jardin d'éden. Nous jardinerons nus afin de mieux nous connecter avec la nature.",
-    "SOS cucurbitacées ! J'ignore tout du repiquage de ces délicieux arômes, une âme charitable pour m'aider ?"
-  ].sample,
-  orientation: %w[Nord Sud Est Ouest].sample,
-  floor_type: ['Pauvre et humide', 'Riche et humide', 'Pauvre et sec',
-               'Riche et sec'].sample,
+  title: 'Petit balcon cat-friendly dans les Yvelines',
+  description: "Je cherche un petit coup de main pour élaborer un petit potager et fleurir mon balcon.
+  Petite contrainte : mes deux chats qui doivent pouvoir grignoter allègrement les feuilles sans
+  tomber malade.",
+  orientation: 'Sud',
+  floor_type: 'Terreau',
   is_available: Faker::Boolean.boolean,
   parking: Faker::Boolean.boolean,
   tools_available: Faker::Boolean.boolean,
-  surface: Faker::Number.between(from: 10, to: 300),
-  street_number: Faker::Number.between(from: 1, to: 150),
-  street_name: Faker::Address.street_name,
+  surface: '3',
+  street_number: '62',
+  street_name: 'Résidence Elysée 2',
   zip_code: '78172',
-  city: Faker::Address.city,
-  country: Faker::Address.country,
-  user_id: 1,
-  latitude: Faker::Address.latitude,
-  longitude: Faker::Address.longitude
+  city: 'La Celle-Saint-Cloud',
+  country: 'France',
+  user_id: 1
 )
 
-20.times do
+5.times do
   user = User.new(email: "#{Faker::Name.first_name}@yopmail.com", password: 'testtest',
                   password_confirmation: 'testtest')
   user.save!
@@ -54,7 +46,13 @@ tp User.all
 
 20.times do
   Garden.create!(
-    title: "Jardin de #{Faker::Superhero.name} besoin d'aide urgente pour planter des choux",
+    title: [
+      'Joli petit jardinet',
+      'Magnifique jardin de roses',
+      'Terrasse laissée au dépourvu',
+      'Splendide Eden',
+      'SOS balcon en détresse'
+    ].sample,
     description: [
       "Charmant jardin à partager, rempli de camélias et myosotis.
       A besoin d'une main experte pour m'aider à les chouchouter",
@@ -76,9 +74,7 @@ tp User.all
     zip_code: '78172',
     city: Faker::Address.city,
     country: Faker::Address.country,
-    user_id: User.ids.sample,
-    # latitude: Faker::Address.latitude,
-    # longitude: Faker::Address.longitude
+    user_id: User.ids.sample
   )
 end
 
