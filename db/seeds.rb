90f8e843a32c253dd0fae22617c09415108f1130
Clem-Svg <clementine.fouque@gmail.com>
2022-08-03 15:25:37 +0200

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
+require 'faker'
+
 User.destroy_all
 Garden.destroy_all
 
-# if Doorkeeper::Application.count.zero?
-#   Doorkeeper::Application.create!(name: 'React',
-#                                   redirect_uri: 'http://localhost:3000/', scopes: '')
-# end
+if Doorkeeper::Application.count.zero?
+  Doorkeeper::Application.create!(name: 'React',
+                                  redirect_uri: 'http://localhost:3000/', scopes: '')
+end
 
 User.first_or_create(
   email: 'test@test.test',
@@ -15,53 +17,42 @@ User.first_or_create(
 )
 
 Garden.create!(
-  title: 'Petit balcon cat-friendly dans les Yvelines',
-  description: "Je cherche un petit coup de main pour élaborer un petit potager et fleurir mon balcon.
-  Petite contrainte : mes deux chats qui doivent pouvoir grignoter allègrement les feuilles sans
-  tomber malade.",
-  orientation: 'Sud',
-  floor_type: 'Terreau',
-  is_available: true,
-  parking: true,
-  tools_available: false,
-  surface: '3',
-  street_number: '62',
-  street_name: 'Résidence Elysée 2',
+  title: "Jardin de #{Faker::Superhero.name} besoin d'aide urgente pour planter des choux",
+  description: [
+    "Charmant jardin à partager rempli de camélias et myosotis.
+    Besoin d'une main experte pour m'aider à les chouchouter",
+    "Jardin dans un état déplorable, laissé à l'abandon par mes parents dans les années 70. J'ai besoin d'aide ASAP",
+    'Je voudrais planter un potager dans ce jardinet. Hors de question de remettre les pieds dans un supermarché',
+    "Besoin d'aide pour entretenir ce jardin d'éden. Nous jardinerons nus afin de mieux nous connecter avec la nature.",
+    "SOS cucurbitacées ! J'ignore tout du repiquage de ces délicieux arômes, une âme charitable pour m'aider ?"
+  ].sample,
+  orientation: %w[Nord Sud Est Ouest].sample,
+  floor_type: ['Pauvre et humide', 'Riche et humide', 'Pauvre et sec',
+               'Riche et sec'].sample,
+  is_available: Faker::Boolean.boolean,
+  parking: Faker::Boolean.boolean,
+  tools_available: Faker::Boolean.boolean,
+  surface: Faker::Number.between(from: 10, to: 300),
+  street_number: Faker::Number.between(from: 1, to: 150),
+  street_name: Faker::Address.street_name,
   zip_code: '78172',
-  city: 'La Celle-Saint-Cloud',
-  country: 'France',
+  city: Faker::Address.city,
+  country: Faker::Address.country,
   user_id: 1
 )
 
-user = User.new(email: 'hello@yopmail.com', password: 'testtest',
-                password_confirmation: 'testtest')
-user.save!
-
-user1 = User.new(email: 'hello1@yopmail.com', password: 'testtest',
-                 password_confirmation: 'testtest')
-user1.save!
-
-user2 = User.new(email: 'hello2@yopmail.com', password: 'testtest',
-                 password_confirmation: 'testtest')
-user2.save!
-
-user3 = User.new(email: 'hello3@yopmail.com', password: 'testtest',
-                 password_confirmation: 'testtest')
-user3.save!
-
+20.times do
+  user = User.new(email: "#{Faker::Name.first_name}@yopmail.com", password: 'testtest',
+                  password_confirmation: 'testtest')
+  user.save!
+end
 puts '*' * 20
 puts 'USERS'
-# tp User.all
+tp User.all
 
-10.times do
+20.times do
   Garden.create!(
-    title: [
-      'Joli petit jardinet',
-      'Magnifique jardin de roses',
-      'Terrasse laissée au dépourvu',
-      'Splendide Eden',
-      'SOS balcon en détresse'
-    ].sample,
+    title: "Jardin de #{Faker::Superhero.name} besoin d'aide urgente pour planter des choux",
     description: [
       "Charmant jardin à partager, rempli de camélias et myosotis.
       A besoin d'une main experte pour m'aider à les chouchouter",
@@ -74,19 +65,21 @@ puts 'USERS'
     orientation: %w[Nord Sud Est Ouest].sample,
     floor_type: ['Pauvre et humide', 'Riche et humide', 'Pauvre et sec',
                  'Riche et sec'].sample,
-    is_available: [true, false].sample,
-    parking: [true, false].sample,
-    tools_available: [true, false].sample,
-    surface: [15, 20, 3, 9, 78].sample,
-    street_number: [15, 20, 3, 9, 78].sample,
-    street_name: 'rue Victor Hugo',
-    zip_code: '75011',
-    city: ['Paris', 'Rueil Malmaison', 'Versailles', 'Boulogne'].sample,
-    country: 'France',
-    user_id: User.ids.sample
+    is_available: Faker::Boolean.boolean,
+    parking: Faker::Boolean.boolean,
+    tools_available: Faker::Boolean.boolean,
+    surface: Faker::Number.between(from: 10, to: 300),
+    street_number: Faker::Number.between(from: 1, to: 150),
+    street_name: Faker::Address.street_name,
+    zip_code: '78172',
+    city: Faker::Address.city,
+    country: Faker::Address.country,
+    user_id: User.ids.sample,
+    # latitude: Faker::Address.latitude,
+    # longitude: Faker::Address.longitude
   )
 end
 
 puts '*' * 20
 puts 'GARDENS'
-# tp Garden.all
+tp Garden.all
