a12db6c7327297864cb3641978bd03d8a5802414
Clem-Svg <clementine.fouque@gmail.com>
2022-09-06 00:58:03 +0200

    adding vercel domain to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://coup-de-pousse-deploy-mb1leuckk-clem-svg.vercel.app/"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    origins 'https://coup-de-pousse-deploy.vercel.app'
+    resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
   end
 end
